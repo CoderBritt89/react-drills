a5c3df6e01f1db0dabaa18cd795e254d2cd7bc13
CoderBritt89 <72882552+CoderBritt89@users.noreply.github.com>
2021-01-21 15:42:29 -0700

    coming back to this later

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+constructor(){
+super()
+this.state = {
+  favoriteFood: ['Poke', 'Pizza', 'Burger', 'Quesadilla', 'Sushi', 'Ramen', 'Pho', 'Bahn Mi', 'Croissants', 'salmon', 'steak'],
+  filterString: ''
+
 }
+
+}
+
+
+handleChange(filter){
+  this.setState({filterString: filter})
+
+}
+
+filterList(userInput){
+ let foodList = this.state.favoriteFood;
+ let filteredList = [];
+
+
+ for(let i = 0; i<foodList.length; i++ )
+ if(foodList[i].includes(userInput)){
+   filteredList.push(foodList[i])
+ }
+
+}
+
+
+render (){
+
+  let list = this.state.favoriteFood.map((elem, index)=>{
+    return <h2 key={index}>{elem}</h2>
+
+  });
+
+  return(
+
+    <div>
+      <input onChange={(e)=>this.filterList(e.target.value)} type="text" />
+      <h2>{list}</h2>
+
+    </div>
+  )
+}
+
+}
+
+
+
 
 export default App;
