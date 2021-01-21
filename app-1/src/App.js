6a87fb0e8e8a2c63384e4acd2610d3afdfd254ff
CoderBritt89 <72882552+CoderBritt89@users.noreply.github.com>
2021-01-21 14:43:45 -0700

    finished

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+class App extends Component{
+constructor(){
+  super()
+  this.state = {
+    message: ''
+
+
+  }
+}
+
+
+displayMessage(str){
+  this.setState({message: str})
+
+}
+
+
+render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input placeholder="type" onChange={(e)=>this.displayMessage(e.target.value)}/>
+      <p>{this.state.message}</p>
     </div>
-  );
+  )
+
+
+
+}
+ 
 }
 
 export default App;
