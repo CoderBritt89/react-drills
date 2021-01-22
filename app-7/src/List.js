5035f1e254ba3ab3b814e4c2c4406fa41e4026bd
CoderBritt89 <72882552+CoderBritt89@users.noreply.github.com>
2021-01-21 17:53:59 -0700

    saving work

---
+++ app-7/src/List.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class List extends Component{
+constructor(){
+super()
+
+
+}
+
+   render(){
+       return(
+           <div>
+
+           </div>
+       )
+   } 
+
+}
+
+export default List;
---
+++ app-7/src/NewTask.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+class NewTask extends Component{
+constructor(){
+    super()
+    this.state = {
+
+    }
+}
+
+
+
+render (){
+    return(
+        <div></div>
+    )
+}    
+}
+
+export default NewTask;
